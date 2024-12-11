3286aa1272276949d3b0f63976097ce6a862fdcb
sasanklucky <sasanksekharbaral123@gmail.com>
2024-12-11 13:32:07 +0530

    referance2

--- custom_addons/orm_hospitals/models/patient.py
+++ custom_addons/orm_hospitals/models/patient.py
@@ -10,7 +10,7 @@ class HospitalPatient(models.Model):
 
     name = fields.Char(string="Name")
     age = fields.Integer(string="Age")
-    ref = fields.Char(string="Reference")
+    ref = fields.Char(string="Reference2")
     gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender", default="male")
     active = fields.Boolean(string="Active", default=True)  # unarchived state
 
